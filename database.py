a3db39ac68184d6b656c9f91c405a2c7110582df
Aadi Bajpai <hello@aadibajpai.com>
2020-09-18 11:38:47 +0530

    add db tooling

--- database.py
+++ database.py
@@ -1,6 +1,14 @@
+import os
+
 import sqlalchemy
 from databases import Database
+from dotenv import load_dotenv
 from sqlalchemy.schema import CreateTable
+
+# load environment variables from .env
+load_dotenv()
+
+DATABASE_URL = f"postgresql://{os.environ['DB_USER']}:{os.environ['DB_PASSWD']}@localhost/vh"
 
 meta = sqlalchemy.MetaData()
 
@@ -8,11 +16,20 @@ meta = sqlalchemy.MetaData()
 Hacker = sqlalchemy.Table(
     "hackers",
     meta,
-    sqlalchemy.Column("id", sa.BigInteger, primary_key=True, nullable=False),
-    sqlalchemy.Column("level", sa.BigInteger, nullable=False, server_default="0"),
+    sqlalchemy.Column("id", sqlalchemy.BigInteger, primary_key=True, nullable=False),
+    sqlalchemy.Column("name", sqlalchemya.String(length=40), nullable=False),  # just for convenience
+    sqlalchemy.Column("level", sqlalchemy.BigInteger, nullable=False, server_default="0"),
 )
 
 ENGINE = None
+
+"""
+ you should manually initialize the db for first run since it's a sync call
+ it will create the table locally
+ >>> from database import meta, DATABASE_URL
+ >>> import sqlalchemy
+ >>> meta.create_all(sqlalchemy.create_engine(DATABASE_URL))
+"""
 
 
 async def prepare_engine():
@@ -21,4 +38,3 @@ async def prepare_engine():
         ENGINE = Database(DATABASE_URL)
         await ENGINE.connect()
     return ENGINE
-
