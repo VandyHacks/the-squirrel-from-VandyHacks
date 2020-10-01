4a55c160260abe97c8df48549965b64db1812349
Aadi Bajpai <hello@aadibajpai.com>
2020-10-01 19:30:28 +0530

    another one

--- cogs/times.py
+++ cogs/times.py
@@ -16,7 +16,7 @@ nash = partial(dt.now, tz=cst)  # gives current time in nashville, use instead o
 # Oct 2-4, 2020
 sched = {
     2: [
-        ('4:30pm', 'Team Matching - Glimpse Session'),
+        ('4:30 pm', 'Team Matching - Glimpse Session'),
         ('6:00 pm', 'Opening Ceremony'),
         ('7:00 pm', 'Keynote Speaker - Authors of Swipe to Unlock: Business Strategy for Technologists'),
         ('7:50 pm', 'Keynote Speaker - Jeffrey Rothschild'),
