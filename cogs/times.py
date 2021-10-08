d2780e405d6ae22e410deb74e87c16f21b0e47da
Daniel Li <daniel442li@gmail.com>
2021-10-08 13:50:10 -0500

    update schedule (#14)
    * Hot reload :)
    
    * im really stupid
    
    * im stupid again
    
    * linting lol
    
    * omg linting again
    
    * black linting
    
    * proc
    
    * heroku db
    
    * changed guilds and values for this year's hackathon
    
    * changes VII to VIII
    
    * errrrrr
    
    * ye
    
    * no ds_store
    
    * updated req
    
    * format
    
    * sorted
    
    * links
    
    * oop
    
    * lint
    
    * black
    
    * Fix time sorting
    
    * Fix formatting
    
    * Fix flake8
    
    * More flake8
    
    * Redo black
    
    * Do black with 120
    
    * Style changes
    
    * Fix request
    
    * Actually fix
    
    * remove prev sched declaration
    
    Co-authored-by: Aadi Bajpai <hello@aadibajpai.com>
    Co-authored-by: Akash Munagala <akash.munagala@gmail.com>

--- cogs/times.py
+++ cogs/times.py
@@ -5,6 +5,9 @@ from utils import paginate_embed
 
 import discord
 from discord.ext import commands
+import requests
+from dateutil import parser
+from operator import itemgetter
 
 """
 customise these initial few variables according to
@@ -16,60 +19,45 @@ cst = tz(timedelta(hours=-5))  # cst is 5h behind utc
 
 # hackathon start and end times
 start = dt.fromtimestamp(1633744800, tz=cst)  # 9pm cst oct 8 2021
-end = dt.fromtimestamp(1633874400, tz=cst)  # 9am cst oct 4 2020
+end = dt.fromtimestamp(1633874400, tz=cst)  # 9am cst oct 10 2020
 
 nash = partial(dt.now, tz=cst)  # gives current time in nashville, use instead of dt.now() for uniformity
 
-# Oct 2-4, 2020
-# event format is (time, event name, event link if available)
-sched = {
-    2: [
-        ("4:30 pm", "Team Matching - Glimpse Session", ""),
-        ("6:00 pm", "Opening Ceremony", "https://www.twitch.tv/vandyhacks"),
-        (
-            "7:00 pm",
-            "Keynote Speaker - Authors of Swipe to Unlock: Business Strategy for Technologists",
-            "https://vanderbilt.zoom.us/j/99212620856",
-        ),
-        ("7:50 pm", "Keynote Speaker - Jeffrey Rothschild", "https://www.twitch.tv/vandyhacks"),
-        ("8:30 pm", "Team Matching - Glimpse Session", "https://app.joinglimpse.com/room?key=FDC34E"),
-        ("9:00 pm", "Hacking begins", ""),
-        ("9:00 pm", "Full-Stack Workshop w/ Angular", "https://youtu.be/peKt6tRog60"),
-        ("9:00 pm", "Google Cloud Tech Talk - Cloud Hero Workshop", "https://youtu.be/lrEYwt9ZpyQ"),
-        ("10:00 pm", "Intro to Open Source Workshop", "https://youtu.be/0fkE7Awceig"),
-        ("11:00 pm", "SlackBot Workshop", "https://youtu.be/12lSoyqgSxw"),
-    ],
-    3: [
-        ("8:00 am", "Neural Networks Workshop", "https://youtu.be/Yue_yX5k-Hc"),
-        ("9:00 am", "React Native Workshop", "https://youtu.be/e2JHoda3RZU"),
-        ("10:00 am", "Big Data Workshop", "https://youtu.be/txRjZvOAMnc"),
-        ("11:00 am", "Google Cloud Workshop", "https://youtu.be/D_H4vchrSn8"),
-        ("1:00 pm", "Sponsor Career Fair", ""),
-        ("3:30 pm", "Zoomba", "https://vanderbilt.zoom.us/j/96037192950?pwd=RjhmUDVwTEV5QUNCbUc2SXZtZFlaQT09"),
-        ("4:00 pm", "Exploring Data-Driven Advocacy - The % Project", ""),
-        ("4:30 pm", "MLH Werewolf", ""),
-        ("5:00 pm", "Let's Bake Together!", ""),
-        ("6:00 pm", "MLH Capture The Flag", ""),
-        ("6:30 pm", "Keynote Speaker - Karl Mehta", "https://www.twitch.tv/vandyhacks"),
-        ("7:20 pm", "Skribbl.io", ""),
-        ("8:00 pm", "Keynote Speaker - Shauna McIntyre", "https://www.twitch.tv/vandyhacks"),
-        ("9:00 pm", "Typing Competition!", ""),
-        ("10:30 pm", "Guided meditation and mindfulness", ""),
-        ("11:30 pm", "How to solve a Rubik's cube?", ""),
-    ],
-    4: [
-        ("8:00 am", "How To Demo Workshop", ""),
-        ("8:30 am", "Make your Demo!", ""),
-        ("9:00 am", "Hacking Ends", ""),
-        ("9:30 am", "Finish your Demo and submit to Devpost!", ""),
-        ("9:45 am", "Skribbl.io", ""),
-        ("10:15 am", "How To Judge Workshop", ""),
-        ("10:30 am", "Keynote Speaker - Thiago Olson", "https://www.twitch.tv/vandyhacks"),
-        ("10:30 am", "Judging", ""),
-        ("1:20 pm", "Keynote Speaker - Jennison Asuncion", "https://www.twitch.tv/vandyhacks"),
-        ("3:00 pm", "Closing Ceremony", "https://www.twitch.tv/vandyhacks"),
-    ],
-}
+# Oct 8-10, 2021
+
+url = "https://apply.vandyhacks.org/api/manage/events/pull"
+
+response = requests.get(url)
+
+data = response.json()
+
+friday = []
+saturday = []
+sunday = []
+
+for event in data:
+    today = parser.parse(event["startTimestamp"])
+    today = today - timedelta(hours=5)
+    time = today.time()
+    if event["location"].startswith("https"):
+        eventTuple = (time, event["name"], event["location"])
+    else:
+        eventTuple = (time, event["name"], "")
+    if today.weekday() == 4:
+        friday.append(eventTuple)
+    elif today.weekday() == 5:
+        saturday.append(eventTuple)
+    else:
+        sunday.append(eventTuple)
+
+friday = sorted(friday, key=itemgetter(0))
+saturday = sorted(saturday, key=itemgetter(0))
+sunday = sorted(sunday, key=itemgetter(0))
+friday = [(time.strftime("%I:%M %p"), name, loc) for time, name, loc in friday]
+saturday = [(time.strftime("%I:%M %p"), name, loc) for time, name, loc in saturday]
+sunday = [(time.strftime("%I:%M %p"), name, loc) for time, name, loc in sunday]
+
+sched = {8: friday, 9: saturday, 10: sunday}
 
 
 def time_left(event):
@@ -103,7 +91,7 @@ class Times(commands.Cog):
         else:
             # compose string accordingly
             breakdown = (
-                "VandyHacks VII " + ("begins " if start > nash() else "ends ") + "in " + time_left(event) + " bb"
+                "VandyHacks VIII " + ("begins " if start > nash() else "ends ") + "in " + time_left(event) + " bb"
             )
 
         await ctx.send(breakdown)
@@ -114,7 +102,7 @@ class Times(commands.Cog):
 
         for day, events in sched.items():
             if day >= nash().day:
-                full_day = ["Friday", "Saturday", "Sunday"][day - 2]  # 2 since that was the first day
+                full_day = ["Friday", "Saturday", "Sunday"][day - 8]  # 2 since that was the first day
 
                 embed = discord.Embed(
                     title="VandyHacks VIII Schedule :scroll:",
@@ -126,7 +114,7 @@ class Times(commands.Cog):
                     event_time, event_name, link = event
                     # unapologetically use walrus operator
                     if (
-                        left := dt.strptime(f"2020 Oct {day} {event_time}", "%Y %b %d %I:%M %p").replace(tzinfo=cst)
+                        left := dt.strptime(f"2021 Oct {day} {event_time}", "%Y %b %d %I:%M %p").replace(tzinfo=cst)
                     ) > nash():  # check if event hasn't already passed
 
                         embed.add_field(
