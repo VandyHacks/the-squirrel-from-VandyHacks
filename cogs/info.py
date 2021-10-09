30565a8a087e65bd7a16270b96d463bd70c19477
Aadi Bajpai <hello@aadibajpai.com>
2021-10-08 23:19:01 -0500

    minor vh where fix

--- cogs/info.py
+++ cogs/info.py
@@ -70,7 +70,7 @@ class Info(commands.Cog):
 
     @commands.command(name="where")
     async def vh_where(self, ctx, *, text=None):
-        if "flag" in text and "please" in text:
+        if text and "flag" in text and "please" in text:
             # for quest
             await ctx.author.send("here you go brother vh{here_brother}")
         await ctx.send(
